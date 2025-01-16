ede3ab91493df28a485855b8d6838e1602ee4bcd
Akshat Agrawal <agrawalakshat582@gmail.com>
2025-01-16 17:18:23 +0530

    ghj

--- frontend/src/components/Admin/AdminNavbar.js
+++ frontend/src/components/Admin/AdminNavbar.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "./AdminNavbar.css";
 import logo from "../../images/Pink Modern Simple Bakery Logo (1) 1.png";
 import admin from "../../images/Group (1).png";
 import search from "../../images/Group.png"; // Import search icon
+import hamburgerIcon from "../../images/remove-icon.png"; // Add a hamburger icon image
 
 const AdminNavbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
   const navigate = useNavigate();
+
+  // Toggle hamburger menu
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
 
   // Navigate to My Account page
   const handleAdminClick = () => {
@@ -21,7 +28,11 @@ const AdminNavbar = () => {
           <span className="admin-text">Admin</span>
         </Link>
       </div>
-      <ul className="admin-navbar-links">
+
+      {/* Hamburger Icon */}
+      
+
+      <ul className={`admin-navbar-links ${menuOpen ? "open" : ""}`}>
         <li>
           <Link to="/admin/orders">Orders</Link>
         </li>
@@ -35,10 +46,18 @@ const AdminNavbar = () => {
           <Link to="/admin/customers">Customers</Link>
         </li>
       </ul>
+
       <div className="admin-navbar-icons">
         <img src={search} alt="Search Icon" className="admin-navbar-search-icon" />
         <img src={admin} alt="Admin Icon" onClick={handleAdminClick} />
+        <button className="admin-hamburger" onClick={toggleMenu}>
+        <div className="admin-hamburger-line"></div>
+        <div className="admin-hamburger-line"></div>
+        <div className="admin-hamburger-line"></div>
+      </button>
       </div>
+
+     
     </nav>
   );
 };
